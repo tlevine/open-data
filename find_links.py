fe4af16f6ae746dfb14588268ccd22ec67c28e09
Thomas Levine <occurrence@thomaslevine.com>
2013-12-22 23:26:20 +0000

    test the socrata blah

--- find_links.py
+++ find_links.py
@@ -1,9 +1,11 @@
 def socrata(view):
     is_href = view["viewType"] == "href"
-    links = view['metadata']['accessPoints'].values()
+    links = view.get('metadata', {}).get('accessPoints', {}).values()
     return is_href, links
 
 if __name__ == '__main__':
     import json
-    for view in json.load(open('downloads/socrata/data.cityofchicago.org/api/views\?page\=1')):
-        print(socrata(view))
+    for view in json.load(open('downloads/socrata/data.cityofchicago.org/api/views?page=1')):
+        is_href, links = socrata(view)
+        if is_href:
+            print(links)
