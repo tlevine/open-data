28365133ab18c2f99245710f2441aa50856152b6
Thomas Levine <occurrence@thomaslevine.com>
2013-12-22 20:48:25 +0000

    ckan download thing

---
+++ download_ckan.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from time import sleep
+import os, json
+
+import ckanapi # https://twitter.com/CKANproject/status/378182161330753536
+
+import portals
+
+INSTANCES = 'http://instances.ckan.org/config/instances.json'
+
+def download(portal_url, directory):
+    '''
+    Args:
+        portal: A string for the root of the portal (like "http://demo.ckan.org")
+        directory: The directory to which stuff should be saved
+    Returns:
+        Nothing
+    '''
+
+    # Make sure the directory exists.
+    try:
+        os.makedirs(directory)
+    except OSError:
+        pass
+
+    portal = ckanapi.RemoteCKAN("http://" + portal_url)
+
+    try:
+        datasets = portal.action.package_list()
+    except:
+        print '**Error searching %s**' % portal_url
+        return
+
+    for dataset in datasets:
+        filename = os.path.join(directory, portal_url, dataset)
+        if os.path.exists(filename):
+            pass # print 'Already downloaded %s from %s' % (dataset, portal_url)
+        else:
+            print '  Downloading %s from %s' % (dataset, portal_url)
+            dataset_information = portal.action.package_show(id = dataset)
+            fp = open(filename, 'w')
+            json.dump(dataset_information, fp)
+            fp.close()
+            sleep(3)
+    print '**Finished downloading %s**' % portal_url
---
+++ readme.md
@@ -0,0 +1,1 @@
+Find dead links in data catalogs.
