83dc3287e8dc6d9df7aca7f908d82313654c1ab4
Thomas Levine <occurrence@thomaslevine.com>
2013-12-22 21:53:50 +0000

    requirements.txt

--- download_ckan.py
+++ download_ckan.py
@@ -2,11 +2,10 @@
 from time import sleep
 import os, json
 
+from get import get
 import ckanapi # https://twitter.com/CKANproject/status/378182161330753536
 
-import portals
-
-INSTANCES = 'http://instances.ckan.org/config/instances.json'
+instances = [i['url'] for i in json.loads(get('http://instances.ckan.org/config/instances.json'))]
 
 def download(portal_url, directory):
     '''
@@ -28,7 +27,7 @@ def download(portal_url, directory):
     try:
         datasets = portal.action.package_list()
     except:
-        print '**Error searching %s**' % portal_url
+        print('**Error searching %s**' % portal_url)
         return
 
     for dataset in datasets:
@@ -36,10 +35,10 @@ def download(portal_url, directory):
         if os.path.exists(filename):
             pass # print 'Already downloaded %s from %s' % (dataset, portal_url)
         else:
-            print '  Downloading %s from %s' % (dataset, portal_url)
+            print('  Downloading %s from %s' % (dataset, portal_url))
             dataset_information = portal.action.package_show(id = dataset)
             fp = open(filename, 'w')
             json.dump(dataset_information, fp)
             fp.close()
             sleep(3)
-    print '**Finished downloading %s**' % portal_url
+    print('**Finished downloading %s**' % portal_url)
