25f92f0d5a6cdd2b37c616382041d27a2154aa78
Thomas Levine <occurrence@thomaslevine.com>
2013-12-22 22:09:02 +0000

    download is working

--- download_ckan.py
+++ download_ckan.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from time import sleep
 import os, json
+import re
 
 from get import get
-import ckanapi # https://twitter.com/CKANproject/status/378182161330753536
 
-def download(portal_url, directory):
+import ckanapi # https://twitter.com/CKANproject/status/378182161330753536
+import ckan.logic
+
+def download(url, directory):
     '''
     Args:
         portal: A string for the root of the portal (like "http://demo.ckan.org")
@@ -13,33 +16,41 @@ def download(portal_url, directory):
     Returns:
         Nothing
     '''
+    url_without_protocol = re.sub(r'^https?://', '', url)
 
     # Make sure the directory exists.
     try:
-        os.makedirs(directory)
+        os.makedirs(os.path.join(directory, url_without_protocol))
     except OSError:
         pass
 
-    portal = ckanapi.RemoteCKAN("http://" + portal_url)
+    # Connect
+    portal = ckanapi.RemoteCKAN(url)
 
+    # Search
     try:
         datasets = portal.action.package_list()
     except:
-        print('**Error searching %s**' % portal_url)
+        print('**Error searching %s**' % url)
         return
 
+    # Metadata files
     for dataset in datasets:
-        filename = os.path.join(directory, portal_url, dataset)
+        filename = os.path.join(directory, url_without_protocol, dataset)
         if os.path.exists(filename):
-            pass # print 'Already downloaded %s from %s' % (dataset, portal_url)
+            pass # print 'Already downloaded %s from %s' % (dataset, url_without_protocol)
         else:
-            print('  Downloading %s from %s' % (dataset, portal_url))
-            dataset_information = portal.action.package_show(id = dataset)
+            print('  Downloading %s from %s' % (dataset, url))
+            try:
+                dataset_information = portal.action.package_show(id = dataset)
+            except ckan.logic.NotAuthorized:
+                print('**Not authorized for %s**' % url)
+                return
             fp = open(filename, 'w')
             json.dump(dataset_information, fp)
             fp.close()
             sleep(3)
-    print('**Finished downloading %s**' % portal_url)
+    print('**Finished downloading %s**' % url)
 
 def main():
     urls = [i['url'] for i in json.loads(get('http://instances.ckan.org/config/instances.json'))]
@@ -48,3 +59,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+#   download('http://bermuda.io/','downloads/ckan')
