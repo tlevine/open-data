fe46e454799fbf3d33e1bcf7cb30e17d6aaa045c
Thomas Levine <occurrence@thomaslevine.com>
2013-12-24 21:03:05 +0000

    /api

--- read.py
+++ read.py
@@ -3,7 +3,12 @@ import os, json
 def catalogs(software):
     return os.listdir(os.path.join('downloads', software))
 
+WEIRD = {
+    'data.sfgov.org':os.path.join('data.sfgov.org','api'),
+}
 def socrata(catalog):
+    if catalog in WEIRD:
+        catalog = WEIRD[catalog]
     directory = os.path.join('downloads','socrata',catalog)
     for filename in os.listdir(directory):
         for view in json.load(open(os.path.join(directory, filename))):
