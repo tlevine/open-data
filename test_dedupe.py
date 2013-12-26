c682c97afc21e524e8da052a4cbc43a2f68a92a9
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 02:12:03 +0000

    test dedupe

---
+++ test_dedupe.py
@@ -0,0 +1,25 @@
+import nose.tools as n
+from socrata_homepages import dedupe
+
+def test_dedupe():
+    datasets1 = [
+        {'id': 'a', 'catalog': 'portal1'},
+        {'id': 'b', 'catalog': 'portal1'},
+        {'id': 'c', 'catalog': 'portal1'}]
+    datasets2 = [
+        {'id': 'g', 'catalog': 'portal2'},
+        {'id': 'h', 'catalog': 'portal2'},
+        {'id': 'c', 'catalog': 'portal2'}]
+    edges = [('portal', 'portal2')]
+    observed = dedupe(datasets1 + datasets2, edges)
+
+    # Sort by id
+    expected = [
+        {'id': 'a', 'catalog': 'portal1'},
+        {'id': 'b', 'catalog': 'portal1'},
+        {'id': 'c', 'catalog': 'portal2'},
+        {'id': 'g', 'catalog': 'portal2'},
+        {'id': 'h', 'catalog': 'portal2'},
+    ]
+    observed_list = list(sorted(observed, key = lambda x: x['id']))
+    n.assert_list_equal(observed_list, expected)
