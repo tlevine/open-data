aac201b96fc6de6abec5615213f9a89e1457fde8
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 06:43:42 +0000

    aoeu

--- reports/socrata-pricing.r
+++ reports/socrata-pricing.r
@@ -1,4 +1,6 @@
 library(sqldf)
+library(ggplot2)
+library(reshape2)
 
 q <- function(sql) {
   sqldf(sql, dbname = '/tmp/open-data.sqlite')
@@ -7,5 +9,12 @@ q <- function(sql) {
 catalogs <- q('SELECT * FROM pricing')
 rownames(catalogs) <- catalogs$catalog
 catalogs[-1] <- as.data.frame(lapply(catalogs[-1],as.integer))
-catalogs$catalog <- NULL
+# catalogs$catalog <- NULL
 catalogs[is.na(catalogs)] <- 0
+
+molten <- melt(catalogs, id.vars = c('catalog','n_datasets'))
+
+p <- ggplot(molten) + aes(x = n_datasets, y = value, label = catalog) +
+  scale_x_log10('Number of datasets') +
+  scale_y_log10('Amount of the variable')
+  facet_wrap(~ variable) + geom_point()
