135550437dfaad0bea0ec8c0cb21abeb43e6717e
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 06:38:57 +0000

    load into r

--- reports/socrata-pricing.r
+++ reports/socrata-pricing.r
@@ -3,3 +3,9 @@ library(sqldf)
 q <- function(sql) {
   sqldf(sql, dbname = '/tmp/open-data.sqlite')
 }
+
+catalogs <- q('SELECT * FROM pricing')
+rownames(catalogs) <- catalogs$catalog
+catalogs[-1] <- as.data.frame(lapply(catalogs[-1],as.integer))
+catalogs$catalog <- NULL
+catalogs[is.na(catalogs)] <- 0
