5d9cc6174f4504942f68d220df8fa26606ade62d
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 04:30:19 +0000

    geo

--- reports/socrata-pricing.r
+++ reports/socrata-pricing.r
@@ -4,9 +4,21 @@ q <- function(sql) {
   sqldf(sql, dbname = '/tmp/open-data.sqlite')
 }
 
+n.datasets <- q('SELECT * FROM socrata_dataset_count')
+
 n.311 <- q('
 SELECT catalog, count(*) \'n_311\'
 FROM socrata_deduplicated
 WHERE name LIKE \'%311%\'
 GROUP BY catalog
 ;')
+
+n.geospatial <- q('
+SELECT catalog, count(*) \'n_geo\' FROM
+(
+  SELECT catalog FROM socrata_deduplicated
+  WHERE viewType = \'geo\'
+  GROUP BY tableId
+)
+GROUP BY catalog;
+')
