d99f71b1e9a541be9b6a10bd160ff03055e0a9c5
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 06:55:17 +0000

    plots

--- reports/socrata-pricing.r
+++ reports/socrata-pricing.r
@@ -12,9 +12,28 @@ catalogs[-1] <- as.data.frame(lapply(catalogs[-1],as.integer))
 # catalogs$catalog <- NULL
 catalogs[is.na(catalogs)] <- 0
 
-molten <- melt(catalogs, id.vars = c('catalog','n_datasets'))
+catalogs$has.forms <- factor(catalogs$n_forms > 0, levels = c(TRUE,FALSE))
+catalogs$has.apis <- factor(catalogs$n_apis > 0, levels = c(TRUE,FALSE))
+catalogs$has.311 <- factor(catalogs$n_311 > 0, levels = c(TRUE,FALSE))
+levels(catalogs$has.forms) <-
+  levels(catalogs$has.apis) <-
+  levels(catalogs$has.311) <-
+  c('Yes','No')
 
-p <- ggplot(molten) + aes(x = n_datasets, y = value, label = catalog) +
-  scale_x_log10('Number of datasets') +
-  scale_y_log10('Amount of the variable')
-  facet_wrap(~ variable) + geom_point()
+molten <- melt(catalogs, variable.name = 'has', value.name = 'yes',
+  measure.vars = grep('has.', names(catalogs), value = TRUE))
+
+p1 <- ggplot(catalogs) + aes(x = n_datasets, y = n_apis, color = n_forms > 0) +
+  scale_x_log10('Number of datasets') + scale_y_log10('Number of APIs') +
+  geom_point(size = 5)
+
+p2 <- ggplot(molten) + aes(x = n_datasets, fill = yes) +
+  facet_wrap(~ has) +
+  geom_bar()
+
+p3 <- ggplot(catalogs) + aes(x = n_forms, y = n_datasets, label = catalog) +
+  facet_grid(has.apis ~ has.311) +
+  scale_y_log10('Number of datasets') + scale_x_log10('Number of formss') +
+  geom_text()
+
+t1 <- table(catalogs$has.forms, catalogs$has.apis, catalogs$has.311)
