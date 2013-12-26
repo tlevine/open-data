3740d2255f00714200bab2208d5456b900ee7ae5
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 00:22:34 +0000

    remove dcat stuff

--- socrata-federation-parse.py
+++ socrata-federation-parse.py
@@ -16,48 +16,45 @@ def parse_source(html):
     url = html.xpath('//meta[@property="og:url"]/@content')[0]
     return unicode(re.match(r'https://([^/]+)/browse/embed', url).group(1))
 
+def homepages():
+    directory = os.path.join('downloads', 'socrata-homepages', 'current', homepage)
+    return (os.path.join(directory, filename) for filename in os.listdir(directory))
+
 def build_network():
-    htmls = [parse(os.path.join('homepages',homepage)).getroot() for homepage in os.listdir('homepages')]
+    htmls = [parse(homepage).getroot() for homepage in homepages()]
     return {
         'edges': reduce(lambda a,b: a + parse_page(b), htmls, []),
         'nodes': map(parse_source, htmls),
     }
 
-def identifiers(dcat):
-    identifiers = Counter([dataset['identifier'] for dataset in dcat])
+def identifiers(datasets):
+    identifiers = Counter([dataset['id'] for dataset in datasets])
     return identifiers
 
-def dedupe(dcat, edges):
+def dedupe(datasets, edges):
     '''
     Args:
-        An iterable of Socrata dcat, each list augmented with a "portal" key
+        An iterable of Socrata dataset metadata, each list augmented with a "portal" key
     Returns:
-        An iterable of dcat, still augmented with the "portal" key
+        An iterable of dataset metadata, still augmented with the "portal" key
 
-    This deduplicates and combines of dcat based on the edges of the federation graph.
+    This deduplicates and combines of dataset metadata based on the edges of the federation graph.
     '''
 
     losing_portals = set([edge[0] for edge in edges])
-    duplicates = set((k for k,v in identifiers(dcat).iteritems() if v > 1))
+    duplicates = set((k for k,v in identifiers(datasets).iteritems() if v > 1))
 
-    for dataset in dcat:
-        if not(dataset['portal'] in losing_portals and dataset['identifier'] in duplicates):
+    for dataset in datasets:
+        if not(dataset['portal'] in losing_portals and dataset['id'] in duplicates):
             yield dataset
 
 def main():
     edges = build_network()['edges']
 
     dt = DumpTruck(dbname = '/tmp/plans.sqlite', adapt_and_convert = True)
-    quasi_dcat_in = dt.execute('SELECT * FROM datasets WHERE software = \'socrata\'')
+    datasets_in = dt.execute('SELECT * FROM datasets WHERE software = \'socrata\'')
+    datasets_out = list(dedupe(datasets_in, edges))
 
-    for dataset in quasi_dcat_in:
-        dataset['identifier'] = dataset['id']
-
-    quasi_dcat_out = list(dedupe(quasi_dcat_in, edges))
-
-    for dataset in quasi_dcat_out:
-        del dataset['identifier']
-
-    dt.create_table(quasi_dcat_out, 'socrata_deduplicated')
+    dt.create_table(datasets_out, 'socrata_deduplicated')
     dt.create_index(['id'], 'socrata_deduplicated', if_not_exists = True, unique = True)
-    dt.upsert(quasi_dcat_out)
+    dt.upsert(datasets_out)
