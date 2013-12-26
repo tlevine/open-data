35a8cc2e43963da0aa45e184ff880da6a92f2a3f
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 06:34:46 +0000

    left join

--- reports/socrata-pricing.sql
+++ reports/socrata-pricing.sql
@@ -1,10 +1,17 @@
-CREATE VIEW IF NOT EXISTS pricing_311 AS
+DROP VIEW IF EXISTS pricing_311;
+DROP VIEW IF EXISTS pricing_geo;
+DROP VIEW IF EXISTS pricing_forms;
+DROP VIEW IF EXISTS pricing_form_tables;
+DROP VIEW IF EXISTS pricing_apis;
+DROP VIEW IF EXISTS pricing;
+
+CREATE VIEW pricing_311 AS
 SELECT catalog, count(*) 'n_311'
 FROM socrata_deduplicated
 WHERE name LIKE '%311%'
 GROUP BY catalog;
 
-CREATE VIEW IF NOT EXISTS pricing_geo AS
+CREATE VIEW pricing_geo AS
 SELECT catalog, count(*) 'n_geo' FROM
 (
   SELECT catalog FROM socrata_deduplicated
@@ -13,13 +20,13 @@ SELECT catalog, count(*) 'n_geo' FROM
 )
 GROUP BY catalog;
 
-CREATE VIEW IF NOT EXISTS pricing_forms AS
+CREATE VIEW pricing_forms AS
 SELECT catalog, count(*) 'n_forms'
 FROM socrata_deduplicated
 WHERE displayType = 'form'
 GROUP BY catalog;
 
-CREATE VIEW IF NOT EXISTS pricing_form_tables AS
+CREATE VIEW pricing_form_tables AS
 SELECT catalog, count(*) 'n_form_tables' FROM
 (
   SELECT catalog FROM socrata_deduplicated
@@ -28,9 +35,25 @@ SELECT catalog, count(*) 'n_form_tables' FROM
 )
 GROUP BY catalog;
 
-CREATE VIEW IF NOT EXISTS pricing_apis AS
+CREATE VIEW pricing_apis AS
 SELECT * FROM (
   SELECT rtrim(catalog,'/') 'catalog', apis 'n_apis' FROM socrata_apis
   WHERE catalog NOT LIKE '%/%'
 )
 WHERE catalog NOT LIKE '%/%';
+
+CREATE VIEW pricing AS
+SELECT
+  socrata_dataset_count.catalog,
+  socrata_dataset_count.datasets 'n_datasets',
+  n_geo,
+  n_311,
+  n_forms,
+  n_form_tables,
+  n_apis
+FROM socrata_dataset_count
+LEFT JOIN pricing_geo ON socrata_dataset_count.catalog = pricing_geo.catalog
+LEFT JOIN pricing_311 ON socrata_dataset_count.catalog = pricing_311.catalog
+LEFT JOIN pricing_forms ON socrata_dataset_count.catalog = pricing_forms.catalog
+LEFT JOIN pricing_form_tables ON socrata_dataset_count.catalog = pricing_form_tables.catalog
+LEFT JOIN pricing_apis ON socrata_dataset_count.catalog = pricing_apis.catalog;
