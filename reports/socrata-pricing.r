8c2d65701ba2afbf83bf9774fcd154ba5d7ac594
Thomas Levine <occurrence@thomaslevine.com>
2013-12-26 02:55:21 +0000

    311

--- reports/socrata-pricing.r
+++ reports/socrata-pricing.r
@@ -3,3 +3,10 @@ library(sqldf)
 q <- function(sql) {
   sqldf(sql, dbname = '/tmp/open-data.sqlite')
 }
+
+n.311 <- q('
+SELECT catalog, count(*) \'n_311\'
+FROM socrata_deduplicated
+WHERE name LIKE \'%311%\'
+GROUP BY catalog
+;')
