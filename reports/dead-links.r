07363815d0c8e3b685c6e42ef013dac47545683c
Thomas Levine <occurrence@thomaslevine.com>
2014-01-08 04:32:21 +0000

    plot

--- reports/dead-links.r
+++ reports/dead-links.r
@@ -11,13 +11,18 @@ FROM links;
 datasets <- with(new.env(), sqldf(sql, dbname = '/tmp/open-data.sqlite'))
 datasets$software <- factor(datasets$software)
 datasets$catalog <- factor(datasets$catalog)
-datasets$alive <- as.logical(datasets$alive)
+datasets$alive.factor <- factor(datasets$alive, levels = 1:0)
+levels(datasets$alive.factor) <- c('Alive','Dead')
 datasets$status_code <- factor(datasets$status_code)
 datasets$status_code[datasets$status_code == '-42'] <- NA
 
 
 
-p <- ggplot(datasets) +
-  aes(x = catalog, fill = alive) +
+p1 <- ggplot(datasets) +
+  aes(x = catalog, y = mean(alive), fill = software) +
+  geom_bar() + coord_flip()
+
+p2 <- ggplot(datasets) +
+  aes(x = catalog, fill = alive.factor) +
   facet_wrap(~ software) +
   geom_bar() + coord_flip()
