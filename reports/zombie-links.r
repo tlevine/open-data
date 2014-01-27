4b3751075bdb29878f8305f23b0d0a57ed815174
Thomas Levine <occurrence@thomaslevine.com>
2014-01-27 18:40:25 +0000

    aoeu

--- reports/zombie-links.r
+++ reports/zombie-links.r
@@ -37,8 +37,41 @@ get.datasets <- function() {
   datasets
 }
 
-if (!all(list('datasets') %in% ls())) {
+if (!all(list('datasets', 'catalogs') %in% ls())) {
   datasets <- get.datasets()
+  catalogs <- sqldf('
+SELECT
+  catalog,
+  avg(status_code == \'Timeout\') \'timeouts\',
+  sum(status_code != \'Not link\') \'links\',
+  count(*) \'datasets\'
+FROM datasets GROUP BY catalog')
 }
+
+p.codes <- ggplot(datasets) + aes(x = status_code) + geom_bar() +
+  xlab('HTTP status code') +
+  scale_y_continuous('Number of datasets', labels = comma) +
+  ggtitle('Which status codes were returned when I checked link liveliness?')
+
+p.data.openva.com <- ggplot(subset(datasets, catalog == 'data.openva.com')) +
+  aes(x = status_code) + geom_bar() +
+  xlab('HTTP status code') +
+  scale_y_continuous('Number of datasets', labels = comma) +
+  ggtitle('Which status codes were returned when I checked link liveliness on data.openva.com?')
+
+p.dati.trentino.it <- ggplot(subset(datasets, catalog == 'dati.trentino.it')) +
+  aes(x = status_code) + geom_bar() +
+  xlab('HTTP status code') +
+  scale_y_continuous('Number of datasets', labels = comma) +
+  ggtitle('Which status codes were returned when I checked link liveliness on dati.trentino.it?')
+
+p.timeouts <- ggplot(catalogs) +
+  aes(x = catalog, y = timeouts, width = links / sum(links)) +
+  geom_bar(stat = 'identity', position = 'identity') +
+  coord_flip()
+
+p.catalogs <- ggplot(catalogs) +
+  aes(x = links, y = timeouts) +
+  geom_point()
 
 # knit('zombie-links.Rmd')
