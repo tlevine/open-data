02890587a0685154094c2d1b1f5645bd666ed623
Thomas Levine <occurrence@thomaslevine.com>
2014-01-29 13:35:38 +0000

    don't print

--- links.py
+++ links.py
@@ -34,20 +34,22 @@ def ckan(dataset):
         'identifier': dataset['name'],
     }
 
-def is_alive(url):
+def is_alive(url, log = False):
     try:
         r = requests.head(url, allow_redirects=True, timeout = 2)
     except:
         status_code = -42
         headers = None
         error = traceback.extract_stack()
-        print('Failed:    ' + url)
+        if log:
+            print('Failed:    ' + url)
     else:
         status_code = r.status_code
         headers = dict(r.headers)
         if 'content-disposition' in headers:
             headers['content-disposition'] = headers['content-disposition'].decode('unicode_escape')
         error = None
-        print('Succeeded: ' + url)
+        if log:
+            print('Succeeded: ' + url)
 
     return status_code, json.dumps(headers), json.dumps(error)
