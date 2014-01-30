71f382158fadb1c825668e0ca08283cb1a79c7d9
Thomas Levine <occurrence@thomaslevine.com>
2014-01-30 04:51:01 +0000

    aoeu

--- reports/zombie-links.r
+++ reports/zombie-links.r
@@ -44,14 +44,30 @@ SELECT
   software,
   catalog,
   sum(status_code == \'Timeout\') \'timeouts\',
-  sum(status_code != \'Not link\') \'links\',
+  sum(status_code == \'Not link\') \'not_links\',
+  sum(status_code != \'Not link\' AND status_code != \'Timeout\' AND status_code = 200) \'live_links\',
+  sum(status_code != \'Not link\' AND status_code != \'Timeout\' AND status_code NOT NULL) \'dead_links\',
   count(*) \'datasets\'
 FROM datasets GROUP BY catalog')
-  catalogs$prop.timeouts <- catalogs$timeouts / catalogs$links
+  catalogs$prop.bad <- catalogs$live_links / (catalogs$datasets - catalogs$not_links)
   catalogs$catalog <- factor(catalogs$catalog,
-    levels = catalogs$catalog[order(catalogs$prop.timeouts, decreasing = TRUE)])
+    levels = catalogs$catalog[order(catalogs$prop.bad, decreasing = TRUE)])
   catalogs
 }
+
+get.link.groupings <- function(catalogs) {
+  for (column in c('timeouts','not_links','live_links','dead_links')){
+    catalogs[,column] <- catalogs[,column] / catalogs$datasets
+  }
+
+  link.groupings <- melt(catalogs,
+    id.vars = c('software','catalog'),
+    measure.vars = c('not_links','live_links','dead_links','timeouts'),
+    variable.name = 'link.type', value.name = c('proportion'))
+
+  link.groupings
+}
+
 
 get.duplicates <- function() {
   sql = '
@@ -66,10 +82,11 @@ GROUP BY catalog, identifier
   unique.links
 }
 
-if (!all(list('datasets', 'catalogs', 'unique.links') %in% ls())) {
-  datasets <- get.datasets()
+if (!all(list('datasets', 'catalogs', 'unique.links', 'link.groupings') %in% ls())) {
+# datasets <- get.datasets()
   catalogs <- get.catalogs(datasets)
   unique.links <- get.duplicates()
+  link.groupings <- get.link.groupings(catalogs)
 }
 
 p.codes <- ggplot(datasets) + aes(x = status_code) + geom_bar() +
@@ -89,8 +106,8 @@ p.dati.trentino.it <- ggplot(subset(datasets, catalog == 'dati.trentino.it')) +
   scale_y_continuous('Number of datasets', labels = comma) +
   ggtitle('Which status codes were returned when I checked link liveliness on dati.trentino.it?')
 
-p.timeouts <- ggplot(subset(catalogs, links > 0)) +
-  aes(x = catalog, y = prop.timeouts, fill = software) +
+p.bad <- ggplot(subset(catalogs, datasets > 0)) +
+  aes(x = catalog, y = prop.bad, fill = software) +
   geom_bar(stat = 'identity') +
   xlab('Data catalog\n(Only data catalogs with externally stored datasets are included.)') +
   scale_y_continuous('Proportion of external datasets that timed out', labels = percent) +
@@ -115,6 +132,15 @@ p.duplicates.socrata <- ggplot(subset(unique.links, software == 'socrata')) +
 unique.links.socrata <- subset(unique.links, software == 'socrata' & is_link)
 table.duplicates.socrata <- table(subset(unique.links, software == 'socrata' & is_link)$n)
 table.duplicates.socrata.by.catalog <- table(unique.links.socrata$catalog, unique.links.socrata$n)
+
+p.link.types <- ggplot(link.groupings) +
+  aes(x = catalog, y = proportion, fill = link.type) +
+  geom_bar(stat = 'identity') + coord_flip() +
+  xlab('') +
+  scale_y_continuous('Proportion of datasets by catalog', labels = percent) +
+  theme(legend.position = 'bottom', axis.text.y = element_text(size = 10)) +
+  scale_fill_discrete('Type of dataset') +
+  ggtitle('Non-links, live links and dead links across data catalogs')
 
 
 # knit('zombie-links.Rmd')
